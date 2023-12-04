acc70cf12a05ddf9616f224f368f7d5d510b7125
mryan-3 <malawaryan@gmail.com>
2023-12-04 23:11:37 +0300

    added a gt request for the api with the multiple lines of code

---
+++ day1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go/scanner"
+	"net/http"
+	"os"
+)
+
+func main(){
+    fmt.Println("Advent of code day one!!")
+    response, _ = http.Get("https://adventofcode.com/2023/day/1/input")
+
+}
