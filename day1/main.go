04436a17fd6f1b50872eab4d8d75ff40ea60c591
mryan-3 <malawaryan@gmail.com>
2023-12-04 23:57:33 +0300

    updated the code but will work on the regex

--- day1/main.go
+++ day1/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"go/scanner"
 	"net/http"
-	"os"
+    "regexp"
+    "strconv"
 )
 
 func main(){
     fmt.Println("Advent of code day one!!")
-    response, _ = http.Get("https://adventofcode.com/2023/day/1/input")
+    response, _ := http.Get("https://adventofcode.com/2023/day/1/input")
+
+    fileScanner := bufio.NewScanner(response.Body)
+    sum := 0
+
+    for fileScanner.Scan(){
+        line := fileScanner.Text()
+        // Extract first and last digits
+        match := regexp.MustCompile(`(\d).*(\d$)`).FindStringSubmatch(line)
+        if len(match) > 1 {
+            firstDigit, lastDigit := match[1], match[2]
+            digits := firstDigit + lastDigit
+            value, _ := strconv.Atoi(digits)
+            sum += value
+        }
+
+    }
+    fmt.Println(sum)
 
 }
