e18690966a306cd7d21d24a52b93026e7fb2a7f1
mryan-3 <malawaryan@gmail.com>
2023-12-05 20:48:46 +0300

    Changed the file to be accessed locally. Made sure it was running! Learnt new stuff

--- day1/go.mod
+++ day1/go.mod
@@ -1,3 +1,5 @@
 module github.com/mryan-3/AOC/day1
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- day1/main.go
+++ day1/main.go
@@ -3,62 +3,61 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"net/http"
+	"log"
+	"os"
 	"strconv"
-	"strings"
 	"unicode"
 )
+func check(e error){
+    if e != nil{
+        panic(e)
+    }
+}
 
 func main(){
-    fmt.Println("Advent of code day one!!")
-    sessionCookie := "53616c7465645f5ffd74c5a04945e53f43ad3b2d016e44a59b5f9c062264d186f003da69cabbc5b8b898b81e964c00db5485b8ce26ba9772b86a11a6728d7fd8"
+    fmt.Println("Advent of code day 1")
 
-	// Make an HTTP GET request to the URL with the session cookie
-	request, _ := http.NewRequest("GET", "https://adventofcode.com/2023/day/1/input", nil)
-	request.AddCookie(&http.Cookie{Name: "session", Value: sessionCookie})
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		fmt.Println("Error fetching calibration document:", err)
-		return
-	}
-    fmt.Println(response)
+    resp, err := os.Open("../day1/input.txt")
+    check(err)
+    defer resp.Close()
 
     sum := 0
-    body, _ := bufio.NewReader(response.Body).ReadString('\n')
-    fmt.Println("response:", body)
-    fileScanner := bufio.NewScanner(strings.NewReader(body))
-    for fileScanner.Scan(){
-        line := fileScanner.Text()
 
-        firstDigit, lastDigit := firstAndlastDigit(line)
-        calibrationValue := combineDigits(firstDigit, lastDigit)
+    scanner := bufio.NewScanner(resp)
 
+    for scanner.Scan(){
+        line := scanner.Text()
+        firstDigit, lastDigit := firstAndLast(line)
+        fmt.Println("Line:", line, "First Digit: ", firstDigit, "Last DIgit: ",lastDigit)
+
+        calibrationValue := combinedValue(firstDigit, lastDigit)
         sum += calibrationValue
-    }
+        fmt.Println(sum)
 
-    fmt.Println("The sum is :", sum)
-}
 
-func firstAndlastDigit(line string)(int, int){
-    firstDigit, lastDigit := 0, 0
-
-    for _, char := range line {
-        if unicode.IsDigit(char){
-            digit, _ := strconv.Atoi(string(char))
-            if firstDigit == 0{
-                firstDigit = digit
-            }
-            lastDigit = digit
+        if err := scanner.Err(); err != nil {
+            log.Fatal(err)
         }
     }
-    if firstDigit == 0 {
-        firstDigit = lastDigit
-    }
-
-    return firstDigit, lastDigit
-
+    fmt.Println("The sum is: ", sum)
 }
-func combineDigits(firstDigit, lastDigit int)int{
+
+func firstAndLast(line string)(int, int){
+    firstDigit, lastDigit := 0, 0
+
+
+        for _, char := range line{
+            if unicode.IsDigit(char){
+                digit, _ := strconv.Atoi(string(char))
+                if firstDigit == 0 {
+                    firstDigit = digit
+                }
+                lastDigit = digit
+            }
+        }
+        return firstDigit, lastDigit
+}
+
+func combinedValue(firstDigit, lastDigit int)int{
     return firstDigit*10 + lastDigit
 }
