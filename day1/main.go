b4f689d99b9dbe0ecca5584de3ed6c8a498c1aff
mryan-3 <malawaryan@gmail.com>
2023-12-05 06:42:16 +0300

    Made good progress, but the response body only returns one value

--- day1/main.go
+++ day1/main.go
@@ -11,14 +11,33 @@ import (
 
 func main(){
     fmt.Println("Advent of code day one!!")
-    response, _ := http.Get("https://adventofcode.com/2023/day/1/input")
+    sessionCookie := "53616c7465645f5ffd74c5a04945e53f43ad3b2d016e44a59b5f9c062264d186f003da69cabbc5b8b898b81e964c00db5485b8ce26ba9772b86a11a6728d7fd8"
 
-    fileScanner := bufio.NewScanner(strings.NewReader(response))
+	// Make an HTTP GET request to the URL with the session cookie
+	request, _ := http.NewRequest("GET", "https://adventofcode.com/2023/day/1/input", nil)
+	request.AddCookie(&http.Cookie{Name: "session", Value: sessionCookie})
 
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		fmt.Println("Error fetching calibration document:", err)
+		return
+	}
+    fmt.Println(response)
 
+    sum := 0
+    body, _ := bufio.NewReader(response.Body).ReadString('\n')
+    fmt.Println("response:", body)
+    fileScanner := bufio.NewScanner(strings.NewReader(body))
+    for fileScanner.Scan(){
+        line := fileScanner.Text()
 
+        firstDigit, lastDigit := firstAndlastDigit(line)
+        calibrationValue := combineDigits(firstDigit, lastDigit)
 
+        sum += calibrationValue
+    }
 
+    fmt.Println("The sum is :", sum)
 }
 
 func firstAndlastDigit(line string)(int, int){
@@ -39,4 +58,7 @@ func firstAndlastDigit(line string)(int, int){
 
     return firstDigit, lastDigit
 
+}
+func combineDigits(firstDigit, lastDigit int)int{
+    return firstDigit*10 + lastDigit
 }
